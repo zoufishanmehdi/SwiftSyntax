055c221e774cc9bb97a160e731a79606ac51e5fa
zoufishanmehdi <zzmehdi@gmail.com>
2016-04-03 15:50:18 -0400

    Optionals

---
+++ Lesson2Optionals.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Foundation
+
+//: # All about Optionals!
+
+//: ## Nil is disallowed in most Swift types
+
+var x: Int
+//x = nil
+
+var c: AnyObject
+//c = UIColor.redColor()
+    ////c = nil
+
+
+//: ## 1. A method that cannot return a value
+
+var y: Int
+var s1: String
+var s2: String
+
+s1 = "123"
+s2 = "ABC"
+
+//y = Int(s1)
+//y = Int(s2)
+
+
+//: ## Declaring optionals with Question Marks
+
+//Example 1
+var z: Int?
+var string: String
+string = "123"
+z = Int(string)
+//z * 2
+
+/*
+//Example 2
+class AnotherViewController: UIViewController {
+    //var anotherButton: UIButton
+}
+*/
+
+
+
+//: ## Unwrapping Optionals with if let
+//Example 1: A scenario that could return an optional with a nil value 
+var zee: Int?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
