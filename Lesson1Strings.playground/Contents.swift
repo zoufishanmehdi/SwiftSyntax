97dc2b6f862533519f8286d9c2ded45b6c0e5128
zoufishanmehdi <zzmehdi@gmail.com>
2016-04-03 12:46:06 -0400

    String and Sorcery

---
+++ Lesson1Strings.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: # Strings
+
+import Cocoa
+import Foundation
+
+
+//: ## Defining Strings using string literals
+
+let myFirstString = "mo 💩"
+                    //Cmmd + Ctrl + Space -> emoji popup
+let mySecondString = "mo problems"
+
+
+//: ## String concatenation
+
+let theTruth = myFirstString + "," + mySecondString
+
+
+//: ## Emoji characters
+
+let similarTruth = "💰 can't buy me 💖"
+
+
+
+//: ## String interpolation
+
+//: ### Plain string 
+
+var doggyDiet = "Lulu eats 251labs of dog food per month"
+
+
