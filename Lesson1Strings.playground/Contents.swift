c6420b3a9ca17d8f3e3ac616a741e7728e6b39d0
zoufishanmehdi <zzmehdi@gmail.com>
2016-04-03 12:57:24 -0400

    More string

--- Lesson1Strings.playground/Contents.swift
+++ Lesson1Strings.playground/Contents.swift
@@ -29,3 +29,45 @@ let similarTruth = "💰 can't buy me 💖"
 var doggyDiet = "Lulu eats 251labs of dog food per month"
 
 
+//: ### String with variables
+
+var dogName = "Ferris"
+doggyDiet = "\(dogName) eats 25lbs of dog food per month"
+
+
+//: ### String with variables and expression
+
+var lbsPerDay = 0.75
+var daysPerMonth:Double = 30.0
+doggyDiet = "\(dogName) eats \(lbsPerDay * daysPerMonth)lbs of dog food per month"
+
+lbsPerDay = 0.25
+dogName = "Lil Frankie"
+doggyDiet =  "\(dogName) eats \(lbsPerDay * daysPerMonth)lbs of dog food per month"
+
+
+
+//: ## More String
+
+//: ### A String is an Array of Characters 
+var password = "Meet me in St. Louis"
+for character in password.characters {
+    if character == "e" {
+        print("found an e!")
+    } else {
+        print("not found")
+    }
+}
+
+//: ## A String can be treated as an NSString
+
+let newPassword = password.stringByReplacingOccurrencesOfString("e", withString: "3")
+
+
+
+
+
+
+
+
+
