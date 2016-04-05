68dae0ab4ac49f44b3d1e7f3d9c94ccb42612833
zoufishanmehdi <zzmehdi@gmail.com>
2016-04-05 16:16:48 -0400

    Collections

---
+++ Lesson3Collections.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+//Array- ordered list of items
+//Dictionary- collection of key-value pairs
+//Set- unordered list of distinct values
+
+//: ### Initializing Arrays
+//The verbose way
+var numbers = Array<Double>()
+
+//More common use
+var moreNumbers = [Double]()
+moreNumbers = [85.0, 90.0, 95.0]
+
+//Array literal syntax
+let differentNumbers = [97.5, 98.5, 99.0]
+
+
