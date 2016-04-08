036a6b6513db5e17091a97d448a145a477f7d8af
zoufishanmehdi <zzmehdi@gmail.com>
2016-04-08 16:24:18 -0400

    filterByYear

--- Lesson4Functions.playground/Contents.swift
+++ Lesson4Functions.playground/Contents.swift
@@ -39,4 +39,66 @@ class Arithmetic {
     }
 }
 
-//
+// func functionName (parameterName: parameterType) -> returnType {
+//        statements to execute
+//        return object
+//     }
+
+
+let stringToReverse = "Mutable or Immutable? That is the question"
+
+func reverseString(stringToReverse: String) {
+    var reversedString = ""
+    for character in stringToReverse.characters {
+        reversedString = "\(character)" + reversedString
+    }
+    print(reversedString)
+}
+
+
+
+
+//    func functionName (externalParameterName localParameterName: parameterType) -> returnType {
+//        statements to execute
+//        return object
+//     }
+
+//: Example 1 - firstCharacterOf
+func firstCharacterOf(word word: String) -> Character {
+    return word[word.startIndex]
+}
+
+firstCharacterOf(word: "Mom")
+
+
+//: Example 2 - placeFirstLetterLast
+func placeFirstLetterLast(var myString: String) -> String {
+    myString.append(firstCharacterOf(word: myString))
+    myString.removeAtIndex(myString.startIndex)
+    return myString
+}
+placeFirstLetterLast("Dad")
+
+
+//: Example 3 - filterByYear
+
+class MovieArchive {
+    func filterByYear(year: Int, movies: Dictionary<String, Int>) -> [String] {
+        var filteredArray = [String]()
+        for (movie, releaseYear) in movies {
+        if year == releaseYear {
+            filteredArray.append(movie)
+        }
+      }
+      return filteredArray
+   }
+}
+
+
+var aiThemedMovies = ["Metropolis":1927, "2001: A Space Odyssey":1968, "Blade Runner":1982, "War Games" : 1983, "Terminator": 1984, "The Matrix":1999, "A.I.":2001, "Her": 2013, "Ex Machina":2015]
+
+var myArchive = MovieArchive()
+myArchive.filterByYear(2013, movies: aiThemedMovies)
+
+
+
